2434e225081f8ea3a5a78844f71afd330cceca7e
Vectoria <135844815+Vectoria@users.noreply.github.com>
2023-10-17 16:49:31 +0100

    Update DLA.java
    mais displays para rodar

--- tp1/java/b/DLA.java
+++ tp1/java/b/DLA.java
@@ -9,17 +9,17 @@ public class DLA implements IProcessingApp {
 
 
     private static final int NUM_WALKERS = 100;
-    private static final int NUM_STEPS_PER_FRAME = 100;
+    private static final int NUM_STEPS_PER_FRAME = 90;
     private List<Walker> walkers;
-    private int parar=0;
+    private int parar = 0;
 
     @Override
     public void setup(PApplet p) {
         walkers = new ArrayList<Walker>();
-        Walker estacionario = new Walker(p.width/2, p.height/2);
+        Walker estacionario = new Walker(p.width / 2, p.height / 2);
         walkers.add(estacionario);
 
-        for(int i = 0; i != NUM_WALKERS; ++i) {
+        for (int i = 0; i != NUM_WALKERS; ++i) {
             Walker w = new Walker(p);
             walkers.add(w);
         }
@@ -29,23 +29,25 @@ public class DLA implements IProcessingApp {
     public void draw(PApplet p, float dt) {
         p.background(255);
 
-        for(int i = 0; i != NUM_STEPS_PER_FRAME; ++i) {
-            for(Walker w : walkers) {
+        for (int i = 0; i != NUM_STEPS_PER_FRAME; ++i) {
+            for (Walker w : walkers) {
                 w.wander(p);
-                parar=w.getParar();
-                w.updateState(walkers,p);
+                parar = w.getParar();
+                w.updateState(walkers, p);
 
 
             }
         }
-        for(int i=0; i< parar;i++){
+        for (int i = 0; i < parar; i++) {
             Walker a = new Walker(p);
-            walkers.add(i,a);
+            walkers.add(i, a);
         }
 
 
-        for(Walker w : walkers) {
-            w.display(p);
+        for (Walker w : walkers) {
+            //w.displayBolas(p);
+            w.displayQuadrado(p);
+            //w.displayTriangulo(p);
 
         }
     }
