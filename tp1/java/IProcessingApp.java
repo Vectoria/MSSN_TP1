b9e85c601bfe7a7e40dff151e2154c2f4a4ad250
deSoos <135845109+deSoos@users.noreply.github.com>
2023-10-21 00:37:15 +0100

    Delete tp1/java/IProcessingApp.java

--- tp1/java/IProcessingApp.java
+++
@@ -1,9 +0,0 @@
-package TP1;
-
-import processing.core.PApplet;
-public interface IProcessingApp {
-    void setup(PApplet parent);
-    void draw(PApplet parent, float dt);
-    void keyPressed(PApplet parent);
-    void mousePressed(PApplet parent);
-}
