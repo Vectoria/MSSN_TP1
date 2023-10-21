7a414f90dc449922c55bd065f210c08bfe7934cc
Vectoria <135844815+Vectoria@users.noreply.github.com>
2023-10-21 03:07:14 +0100

    Update DLA.java
    melhor

--- tp1/java/b/DLA.java
+++ tp1/java/b/DLA.java
@@ -1,6 +1,7 @@
 package tp1.java.b;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +9,15 @@ import java.util.List;
 public class DLA implements IProcessingApp {
 
 
-    private static final int NUM_WALKERS = 100;
+    private static final int NUM_WALKERS = 150;
     private static final int NUM_STEPS_PER_FRAME = 90;
     private List<Walker> walkers;
-    private static int parar = 0;
 
     @Override
     public void setup(PApplet p) {
         walkers = new ArrayList<Walker>();
-        Walker estacionario = new Walker(p.width / 2, p.height / 2);
+        p.fill(231,20,105);
+        Walker estacionario = new Walker(p.width / 2, p.height / 2,p);
         walkers.add(estacionario);
 
         for (int i = 0; i != NUM_WALKERS; ++i) {
@@ -32,23 +33,30 @@ public class DLA implements IProcessingApp {
         for (int i = 0; i != NUM_STEPS_PER_FRAME; ++i) {
             for (Walker w : walkers) {
                 w.wander(p);
-                parar = w.getParar();
+               // parar = w.getParar();
                 w.updateState(walkers, p);
 
-
             }
-        }
-        for (int i = 0; i < parar; i++) {
-            Walker a = new Walker(p);
-            walkers.add(i, a);
+            if(Walker.num_wander<NUM_WALKERS){
+                Walker.num_parados++;
+                int novo=  NUM_WALKERS-Walker.num_wander;
+                Walker w= new Walker( p.width/2,p.height/2,p);
+                for(int idx=0; idx<novo; idx++){
+                    w= new Walker(p);
+                    walkers.add(w);
+                }
+            }
         }
 
 
         for (Walker w : walkers) {
-            w.displayBolas(p);
-           // w.displayQuadrado(p);
+
+           // w.displayBolas(p);
+            w.displayQuadrado(p);
             //w.displayTriangulo(p);
+            System.out.println("parados= "+ Walker.num_parados+ " Wander= " + Walker.num_wander);
 
         }
     }
+
 }
